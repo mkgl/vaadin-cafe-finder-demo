706529bc19d2f9002c16717b8469566d287c42b3
Mikaël Geljić <m.geljic@gmail.com>
2019-01-23 16:29:36 +0700

    Add star-rating renderer for the evaluation column

--- src/main/java/me/mkgl/demo/CafeListView.java
+++ src/main/java/me/mkgl/demo/CafeListView.java
@@ -1,9 +1,13 @@
 package me.mkgl.demo;
 
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.data.provider.DataProvider;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.PageTitle;
@@ -23,7 +27,17 @@ public class CafeListView extends VerticalLayout implements BeforeEnterObserver 
 
         cafeList = new Grid<>();
         cafeList.addColumn(Cafe::getName).setHeader("Café");
-        cafeList.addColumn(Cafe::getEvaluation).setHeader("Evaluation");
+        cafeList.addColumn(new ComponentRenderer<>(cafe -> {
+            Div rating = new Div();
+            for (int i = 0; i < 5; i++) {
+                if (cafe.getEvaluation() <= i) {
+                    rating.add(new Icon(VaadinIcon.STAR_O));
+                } else {
+                    rating.add(new Icon(VaadinIcon.STAR));
+                }
+            }
+            return rating;
+        })).setHeader("Evaluation");
 
         add(new H3("All cafés"), cafeList);
         expand(cafeList);
